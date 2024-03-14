642582ed5782c92e135d200f5d1e3f0de47a1eaa
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-03-14 16:46:58 +0300

    publish photo to Telegram channel.

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from telegram import Bot
 bot_token = "7072799364:AAHgUr88fdlG9zVIJFFSP7X9rVM2DShY3i0"
 bot = Bot(token=bot_token)
 print(bot.get_me())
-bot.send_message(chat_id="@dwnl_img", text="I'm sorry Dave I'm afraid I can't do that.")
+# bot.send_message(chat_id="@dwnl_img", text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_document(chat_id="@dwnl_img", document=open('images/epic_photo_0.png', 'rb'))
